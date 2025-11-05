c5607a072358f4014e57f242fd783e4570023fb1
dnyvarc-cloud <dnyvarc@gmail.com>
2025-11-05 14:32:47 -0700

    adding scrapping

--- app_v3.py
+++ app_v3.py
@@ -1,8 +1,26 @@
 import streamlit as st
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
-st.header('🎈 Welcome to the Celebration App! 🎈')
+url = requests.get(f'https://www.scrapethissite.com/pages/forms/').text
+soup = BeautifulSoup(url, 'lxml')
+#players = soup.find_all('tr')
+#print(players)
+players = soup.find_all('tr')[1:]
+st.write(players[0])
+team_name = []
+years = []
+wins = []
 
-if st.button('🎉 Celebrate!'):
-    st.balloons()
-    st.snow()
-    st.toast('Party time!', icon='🎊')
+for i in players:
+    name = i.find_all('td')[0].text.strip()
+    year = i.find_all('td')[1].text.strip()
+    win = i.find_all('td')[2].text.strip()
+    team_name.append(name)
+    years.append(year)
+    wins.append(win)
+
+    data = pd.DataFrame({'Team Name': team_name, 'Year': years, 'Wins': wins})
+    st.dataframe(data)
